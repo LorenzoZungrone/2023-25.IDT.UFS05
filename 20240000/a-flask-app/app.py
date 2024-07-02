c77e34775b0381302621de21d8c34e795239ef74
LorenzoZungrone <151736012+LorenzoZungrone@users.noreply.github.com>
2024-07-02 13:45:31 +0000

    cambio nome database

--- 20240000/a-flask-app/app.py
+++ 20240000/a-flask-app/app.py
@@ -93,7 +93,7 @@ def main():
             host="its-rizzoli-idt-mysql-49390.mysql.database.azure.com",
             user="psqladmin",
             passwd="H@Sh1CoR3!",
-            database="JeremyWade"
+            database="jeremywade"
         )
         risposta="Connection to MySQL DB successful"
         cursor = connection.cursor()
--- 20240000/a-flask-app/script.sql
+++ 20240000/a-flask-app/script.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS JeremyWade;
-USE JeremyWade;
+CREATE DATABASE IF NOT EXISTS jeremywade;
+USE jeremywade;
 CREATE TABLE employees (
   first_name varchar(45) DEFAULT NULL,
   last_name varchar(45) DEFAULT NULL
