0d4903d77484b177a580e6837f835e8f41f9ea48
LorenzoZungrone <151736012+LorenzoZungrone@users.noreply.github.com>
2024-07-02 13:35:12 +0000

    id mysql

--- 20240000/a-flask-app/app.py
+++ 20240000/a-flask-app/app.py
@@ -10,7 +10,7 @@ def initialize_database():
     try:
         # Connessione senza specificare il database, per permettere la creazione del database stesso
         connection = mysql.connector.connect(
-            host="its-rizzoli-idt-mysql-79834.mysql.database.azure.com",
+            host="its-rizzoli-idt-mysql-49390.mysql.database.azure.com",
             user="psqladmin",
             passwd="H@Sh1CoR3!"
         )
@@ -90,7 +90,7 @@ def main():
     risposta="nessuna risposta"
     try:
         connection = mysql.connector.connect(
-            host="its-rizzoli-idt-mysql-79834.mysql.database.azure.com",
+            host="its-rizzoli-idt-mysql-49390.mysql.database.azure.com",
             user="psqladmin",
             passwd="H@Sh1CoR3!",
             database="JeremyWade"
